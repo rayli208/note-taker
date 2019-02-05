18c22e3c9853fbac713160e54cdc277027d0b0ec
Rayli Raykov <rayli208@gmail.com>
2019-02-04 19:47:06 -0500

    added paths

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -4,13 +4,13 @@ const path = require("path");
 router
   .route("/")
   .get(function(req, res) {
-   // SEND HOME PAGE USING RES.SENDFILE AND PATH.JOIN()
+    res.sendFile(path.join(__dirname, "../../public/home.html"));
   });
 
 router
   .route("/notes")
   .get(function (req, res) {
-    // SEND NOTES PAGE USING RES.SENDFILE AND PATH.JOIN()
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
   });
 
 module.exports = router;
